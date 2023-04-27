0d53529b9f78789b4f1178c56cb758fc91dc199b
0sirion <salvatore.arcuri.93@gmail.com>
2023-04-27 10:16:40 +0200

    s

--- app.py
+++ app.py
@@ -2,12 +2,18 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import streamlit as st
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 
 def main():
     st.title("Ml Exercise")
     path1= "https://frenzy86.s3.eu-west-2.amazonaws.com/python/data/Startup.csv"
+    df1 =pd.read_csv(path1)
+    st.dataframe(df1)
+    st.dataframe(df1.corr())
+
+    fig = plt.figure(figsize=(10,8))
+    sns.heatmap(df1.corr(), annot=True)
 
     
 
